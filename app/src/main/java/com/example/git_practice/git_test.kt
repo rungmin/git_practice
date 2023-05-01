2b5e1f68df89e3ef3adb6e50415549c93efef882
rungmin <skw6109@naver.com>
2023-05-01 19:57:25 +0900

    1

---
+++ app/src/main/java/com/example/git_practice/git_test.kt
@@ -0,0 +1,11 @@
+package com.example.git_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class git_test : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_test)
+    }
+}
